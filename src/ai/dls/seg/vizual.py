34563a50e338f78b37619689a38bb92b900b6cf7
Markus <markus.colab@gmail.com>
2021-08-24 15:53:50 +0200

    small corrections

--- src/ai/dls/seg/vizual.py
+++ src/ai/dls/seg/vizual.py
@@ -6,16 +6,19 @@ def plot_seg(X_val, answer):
     plt.figure(figsize=(18, 6))
     for k in range(5):
         n = min(BATCH_SIZE, 6)
-        plt.subplot(2, n, k + 1)
+        plt.subplot(3, n, k + 1)
         plt.imshow(np.rollaxis(X_val[k].numpy(), 0, 3), cmap='gray')
         plt.title('Real')
         plt.axis('off')
 
-        plt.subplot(2, n, k + 1 + n)
-        print(answer.shape)
-        print(answer[k].shape)
-        print(answer[k, 0].shape)
+        plt.subplot(3, n, k + 1 + n)
         plt.imshow(answer[k, 0], cmap='gray')
+        plt.title('Output')
+        plt.axis('off')
+
+        seg = (answer[k,0] > np.mean(answer[k,0].numpy())).int()
+        plt.subplot(3, n, k + 1 + 2*n)
+        plt.imshow(seg, cmap='gray')
         plt.title('Output')
         plt.axis('off')
     return plt
